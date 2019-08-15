b19f8046e023f896f0ebbfac0fe02e07c6dfc0dc
_rizki_rm <kikirizmau140175@gmail.com>
2019-08-15 11:56:42 +0700

    changing appbar title in detail page

--- lib/view/detail.dart
+++ lib/view/detail.dart
@@ -9,7 +9,7 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail Bahan Makanan'),
+        title: Text('Detail Makanan'),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context, false),
