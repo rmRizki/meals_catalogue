c66801f02c51610436aec4bd6aac8cc11ce3d96c
_rizki_rm <kikirizmau140175@gmail.com>
2019-08-25 09:16:07 +0700

    add new property

--- lib/model/data_detail.dart
+++ lib/model/data_detail.dart
@@ -2,7 +2,7 @@ class Detail {
   List<DetailProperty> detail = [];
 
   Detail.fromJson(Map<String, dynamic> json) {
-    json['meals'].forEach((json){
+    json['meals'].forEach((json) {
       detail.add(DetailProperty.fromJson(json));
     });
   }
@@ -13,12 +13,13 @@ class DetailProperty {
   final String nameDetail;
   final String thumbDetail;
   final String descDetail;
+  final String categoryDetail;
 
-  DetailProperty(
-      this.idDetail, this.nameDetail, this.thumbDetail, this.descDetail);
+  DetailProperty(this.idDetail, this.nameDetail, this.thumbDetail,
+      this.descDetail, this.categoryDetail);
 
   factory DetailProperty.fromJson(Map<String, dynamic> json) {
     return DetailProperty(json['idMeal'], json['strMeal'], json['strMealThumb'],
-        json['strInstructions']);
+        json['strInstructions'], json['strCategory']);
   }
 }
